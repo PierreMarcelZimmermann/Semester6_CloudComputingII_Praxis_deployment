a09f11a358bb0912caa1424544c6ee1e874b34c3
Pierre <pierre.marcel.zimmermann@gmail.com>
2025-03-10 14:53:34 +0100

    flask on public ip

--- deployment/terraform/main.tf
+++ deployment/terraform/main.tf
@@ -77,6 +77,18 @@ resource "azurerm_network_security_group" "my_terraform_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+   security_rule {
+    name                       = "HTTP"
+    priority                   = 1002
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "5000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
 }
 
 resource "azurerm_network_interface" "my_terraform_nic" {
