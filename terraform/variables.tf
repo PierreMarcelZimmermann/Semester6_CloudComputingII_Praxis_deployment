a82a28f22dff942c63f7e1f8273f1bb7c150a04b
PierreMarcelZimmermann <89578769+PierreMarcelZimmermann@users.noreply.github.com>
2025-03-12 19:13:34 +0000

    Azure vm (#5)
    * add private vm to tf
    
    * add public ip option
    
    * add depends statement
    
    * flask on public ip
    
    * add frontend anc change folder
    
    * try something
    
    * change pull to git repo

---
+++ terraform/variables.tf
@@ -0,0 +1,24 @@
+variable "resource_group_location" {
+  description = "region for resource group"
+  type        = string
+  default     = "westeurope"
+}
+
+variable "sku" {
+  description = "SKU for cognitive service"
+  type        = string
+  default     = "S0"
+}
+
+variable "resource_group_name_prefix" {
+  description = "Prefix for the resource group name"
+  type        = string
+  default     = "my-rg" # Falls kein Wert übergeben wird
+}
+
+
+variable "username" {
+  type        = string
+  description = "The username for the local account that will be created on the new VM."
+  default     = "azureadmin"
+}
